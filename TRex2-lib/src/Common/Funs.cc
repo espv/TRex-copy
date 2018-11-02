51fed4abb27cad6aa5f3f3c04b5f4ff2a2c80b90
espen <espen.volnes@gmail.com>
2018-11-02 14:30:25 +0100

    only tracing if DEBUG flag is set

--- TRex2-lib/src/Common/Funs.cc
+++ TRex2-lib/src/Common/Funs.cc
@@ -33,6 +33,7 @@ pthread_mutex_t* traceMutex = new pthread_mutex_t;
 double previous_time;
 void traceEvent(int traceId, int pid, bool reset)
 {
+#ifdef DEBUG
   pthread_mutex_lock(traceMutex);
   auto current_time = std::chrono::system_clock::now().time_since_epoch().count();
   if (reset)
@@ -40,6 +41,7 @@ void traceEvent(int traceId, int pid, bool reset)
   cout << traceId << "-" << pid << "-" << current_time/*-previous_time*/ << std::endl;
   previous_time = current_time;
   pthread_mutex_unlock(traceMutex);
+#endif
 }
 
 bool checkConstraints(PubPkt* event, RulePkt* rule, int state) {
