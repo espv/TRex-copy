acb0a1337b1070fedff783b289f92624b1b67075
Espen Volnes <espen.volnes@gmail.com>
2019-02-25 11:54:08 +0100

    tracing: changed debug printout (11)

--- TRex-Server/src/Main.cpp
+++ TRex-Server/src/Main.cpp
@@ -101,8 +101,8 @@ void HandlePubPacket(const boost::system::error_code&)
 void PublishPackets()
 {
 	while (true) {
-		std::cout << "PublishPackets, size of packetQueue: " << packetQueue.size() << std::endl;
 		if (packetQueue.size() > 0) {
+			std::cout << "PublishPackets, size of packetQueue: " << packetQueue.size() << std::endl;
 			PubPkt *pkt = packetQueue.front();
 			traceEvent(1, 0, true);
 			if (++pktsPublished % 2000 == 0) {
