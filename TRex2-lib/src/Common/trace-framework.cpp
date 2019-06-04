b665973eb7092e74f2cdfd789e36946726bd79f2
Espen Volnes <espen.volnes@gmail.com>
2019-06-04 17:13:10 +0200

    fewer events

--- TRex2-lib/src/Common/trace-framework.cpp
+++ TRex2-lib/src/Common/trace-framework.cpp
@@ -33,7 +33,7 @@ long long first_time = 0;
 
 int tracedEvents = 0;
 
-#define MAX_NUMBER_EVENTS 200000
+#define MAX_NUMBER_EVENTS 60000
 TraceEvent events[MAX_NUMBER_EVENTS];
 
 bool writeTraceToFile = true;
